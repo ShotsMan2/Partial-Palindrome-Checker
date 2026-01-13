120d390e20742dd1cf3fed7f9088d9ecc8615959
Selim Efe <uysalselimefe@gmail.com>
2026-01-13 21:22:49 +0300

    Implement partial palindrome check logic within N-bounds
    Added source code that calculates the scan range [N-1, Len-N]. The algorithm iterates indices inwards from these boundaries to check for symmetrical character equality.

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+        char str[99];
+    int i,n,len,sum=0,j,bas,son,palindrom;
+
+    printf("str girin ");
+    scanf("%s",&str);
+
+    printf("n i girin ");
+    scanf("%d",&n);
+
+    len=0;
+    i=0;
+
+    while(str[i]!='\0'){
+        len++;
+        i++;
+    }
+
+    bas=n-1;
+    son=len-n;
+
+    while(son>bas){
+        if(str[bas]!=str[son]){
+            palindrom=0;
+            break;
+        }
+        son--;
+        bas++;
+    }
+
+    if(palindrom==1){
+        printf("palindromdur.");
+    }
+    else{
+        printf("palindrom degildir.");
+    }
+}
